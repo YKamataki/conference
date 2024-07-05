77f66ae902536e1b2d7ece07556bd03ae705ed73
Yuya <k212302@cc.k.tsukuba-tech.ac.jp>
2024-07-05 16:23:41 +0900

    とりあえずdb.goを作成

---
+++ db.go
@@ -0,0 +1,53 @@
+// Database connection and operations
+// Author: Yuya KAMATAKI
+// LICENSE: MIT
+
+package main
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"os"   // for environment variables
+	"time" // for using date
+)
+
+func ConnectDB() *gorm.DB {
+	// Get DB credentials from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+
+	// Connect to the database
+	dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPass + " dbname=postgres port=" + dbPort + " sslmode=disable TimeZone=Asia/Tokyo"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+type Conference struct {
+	ID          uint
+	Title       string
+	Description string
+	Date        time.Time
+	Passcode    *string
+	CreatedAt   time.Time
+	Presenters  []Presenter
+}
+
+type Presenter struct {
+	ID           uint
+	ConferenceID uint
+	Name         string
+	Topic        *string
+	Order        uint
+}
+
+func MigrateDB(db *gorm.DB) {
+	db.AutoMigrate(Conference{})
+	db.AutoMigrate(Presenter{}).AddForeignKey("conference_id", "conferences(id)", "RESTRICT", "RESTRICT")
+}
